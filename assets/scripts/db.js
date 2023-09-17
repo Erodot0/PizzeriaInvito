025b26dbf524df4e22ce2a5a9865896a2955f2e5
MateaKarst <matea.karst@gmail.com>
2023-09-17 23:00:44 +0200

    Adding the menu

---
+++ assets/scripts/db.js
@@ -0,0 +1,329 @@
+export const menu = [
+    {
+        pizza: {
+            header: "TUTTE LE PIZZE",
+            items: [
+                {
+                    name: "AFFUMICATA",
+                    ingredients: ["Pomodoro", "mozzarella", "pancetta", "scamorza"]
+                },
+                {
+                    name: "AMERICANA",
+                    ingredients: ["Pomodoro", "mozzarella", "wurstel", "patatine"]
+                },
+                {
+                    name: "AMSTERDAM",
+                    ingredients: ["Pomodoro", "mozzarella", "crudo", "rucola", "zola"]
+                },
+                {
+                    name: "ARIZONA",
+                    ingredients: ["Pomodoro", "mozzarella", "patate", "salsiccia"]
+                },
+                {
+                    name: "BOSCAIOLA",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto", "funghi", "salsiccia fresca"]
+                },
+                {
+                    name: "BIANCANEVE",
+                    ingredients: ["Mozzarella", "origano"]
+                },
+                {
+                    name: "BISMARK",
+                    ingredients: ["Pomodoro", "mozzarella", "uovo", "prosciutto"]
+                },
+                {
+                    name: "BRIE E RADICCHIO",
+                    ingredients: ["Pomodoro", "mozzarella", "brie", "radicchio"]
+                },
+                {
+                    name: "BRIE E SPECK",
+                    ingredients: ["Pomodoro", "mozzarella", "brie", "speck"]
+                },
+                {
+                    name: "BUFALA",
+                    ingredients: ["Pomodoro", "mozzarella di bufala"]
+                },
+                {
+                    name: "CALABRESE",
+                    ingredients: ["Pomodoro", "mozzarella", "salame piccante"]
+                },
+                {
+                    name: "CAPRICCIOSA",
+                    ingredients: ["Pomodoro", "mozzarella", "funghi", "carciofi", "acciughe", "olive"]
+                },
+                {
+                    name: "CAPRESE",
+                    ingredients: ["Pomodoro", "mozzarella", "pomodoro fresco", "mozzarella di bufala"]
+                },
+                {
+                    name: "CARCIOFI",
+                    ingredients: ["Pomodoro", "mozzarella", "carciofi"]
+                },
+                {
+                    name: "CARPACCIO",
+                    ingredients: ["Pomodoro", "mozzarella", "bresaola", "grana", "rucola"]
+                },
+                {
+                    name: "CODOGNO",
+                    ingredients: ["Pomodoro", "mozzarella", "porcini", "grana"]
+                },
+                {
+                    name: "CORSARA",
+                    ingredients: ["Pomodoro", "mozzarella", "tonno", "cipolla", "zola"]
+                },
+                {
+                    name: "CRUDO",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto crudo"]
+                },
+                {
+                    name: "DANESE",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto", "wurstel"]
+                },
+                {
+                    name: "DIANA",
+                    ingredients: ["Pomodoro", "mozzarella", "carciofi", "porcini"]
+                },
+                {
+                    name: "JACK",
+                    ingredients: ["Pomodoro", "mozzarella", "speck", "scamorza", "zola", "spinaci", "grana"]
+                },
+                {
+                    name: "INCAZZATA",
+                    ingredients: ["Pomodoro", "mozzarella", "salame piccante", "peperoni", "cipolla", "peperoncino"]
+                },
+                {
+                    name: "INCONTRO",
+                    ingredients: ["Mozzarella", "bufala fresca", "rucola", "crudo"]
+                },
+                {
+                    name: "INVITO",
+                    ingredients: ["Pomodoro", "mozzarella", "brie", "speck", "zucchine"]
+                },
+                {
+                    name: "MANGIA E TACI",
+                    ingredients: ["Tutto di tutto"]
+                },
+                {
+                    name: "MARE E MONTI",
+                    ingredients: ["Pomodoro", "mozzarella", "gamberi", "rucola", "funghi porcini"]
+                },
+                {
+                    name: "MARGHERITA",
+                    ingredients: ["Pomodoro", "mozzarella"]
+                },
+                {
+                    name: "MARINARA",
+                    ingredients: ["Pomodoro", "aglio", "origano", "olio"]
+                },
+                {
+                    name: "MEDITERRANEA",
+                    ingredients: ["Pomodoro", "mozzarella", "carciofi", "porcini"]
+                },
+                {
+                    name: "DISCOVOLANTE (doppiapizza)",
+                    ingredients: ["Pomodoro", "mozzarella", "cotto", "funghi", "carciofi", "olive", "salame piccante", "peperoncino"]
+                },
+                {
+                    name: "EGIZIANA",
+                    ingredients: ["Pomodoro", "mozzarella", "melanzane", "zola"]
+                },
+                {
+                    name: "ESPLOSIVA",
+                    ingredients: ["Pomodoro", "mozzarella", "cotto", "funghi", "salame piccante", "peperoncino"]
+                },
+                {
+                    name: "ESTIVA",
+                    ingredients: ["Mozzarella", "mozzarella di bufala", "pomodorini", "basilico fresco"]
+                },
+                {
+                    name: "FRUTTI DI MARE",
+                    ingredients: ["Pomodoro", "mozzarella", "frutti di mare"]
+                },
+                {
+                    name: "FUME",
+                    ingredients: ["Pomodoro", "mozzarella", "speck", "scamorza"]
+                },
+                {
+                    name: "FUNGHI",
+                    ingredients: ["Pomodoro", "mozzarella", "funghi"]
+                },
+                {
+                    name: "GUSTOSA",
+                    ingredients: ["Pomodoro", "mozzarella", "speck", "zola"]
+                },
+                {
+                    name: "ROMANA",
+                    ingredients: ["Pomodoro", "mozzarella", "acciughe", "origano", "capperi", "olive"]
+                },
+                {
+                    name: "MONZESE",
+                    ingredients: ["Pomodoro", "mozzarella", "salsiccia fresca", "scamorza"]
+                },
+                {
+                    name: "NAPOLETANA",
+                    ingredients: ["Pomodoro", "mozzarella", "acciughe", "origano"]
+                },
+                {
+                    name: "NORDICA",
+                    ingredients: ["Pomodoro", "mozzarella", "salsiccia", "cipolla", "gorgonzola"]
+                },
+                {
+                    name: "'NDUJA",
+                    ingredients: ["Pomodoro", "mozzarella", "'nduja"]
+                },
+                {
+                    name: "PANTERA ROSA",
+                    ingredients: ["Pomodoro", "mozzarella", "rucola", "gamberetti", "salmone"]
+                },
+                {
+                    name: "PARMA",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto crudo"]
+                },
+                {
+                    name: "SALMONE",
+                    ingredients: ["Pomodoro", "mozzarella", "salmone affumicato", "panna"]
+                },
+                {
+                    name: "SALSICCIA",
+                    ingredients: ["Pomodoro", "mozzarella", "salsiccia"]
+                },
+                {
+                    name: "SAN BIAGIO",
+                    ingredients: ["Pomodoro", "mozzarella", "porcini", "bresaola", "carciofi", "olive", "grana"]
+                },
+                {
+                    name: "SENZA NOME",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto", "brie", "zucchine"]
+                },
+                {
+                    name: "TALEGGIO E PEPERONI",
+                    ingredients: ["Pomodoro", "mozzarella", "taleggio", "peperoni"]
+                },
+                {
+                    name: "TIRAMISU",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto crudo", "rucola"]
+                },
+                {
+                    name: "PATATINE",
+                    ingredients: ["Pomodoro", "mozzarella", "patatine fritte"]
+                },
+                {
+                    name: "PATATOSA",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto cotto", "patatine"]
+                },
+                {
+                    name: "PIZZA PAZZA",
+                    ingredients: ["Pomodoro", "mozzarella", "zucchine", "zola", "cipolle"]
+                },
+                {
+                    name: "PROSCIUTTO",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto cotto"]
+                },
+                {
+                    name: "PRIMAVERA",
+                    ingredients: ["Pomodoro", "mozzarella", "cotto", "salame piccante", "wurstel", "salsiccia"]
+                },
+                {
+                    name: "TONNO",
+                    ingredients: ["Pomodoro", "mozzarella", "tonno"]
+                },
+                {
+                    name: "TONNO E CIPOLLE",
+                    ingredients: ["Pomodoro", "mozzarella", "tonno", "cipolle"]
+                },
+                {
+                    name: "TONNO E PEPERONI",
+                    ingredients: ["Pomodoro", "mozzarella", "tonno", "peperoni"]
+                },
+                {
+                    name: "VEGETARIANA",
+                    ingredients: ["Pomodoro", "mozzarella", "melanzane", "zucchine", "spinaci", "peperoni grigliati"]
+                },
+                {
+                    name: "WURSTEL",
+                    ingredients: ["Pomodoro", "mozzarella di bufala", "ricotta", "grana", "pomodorini", "bresaola", "rucola"]
+                },
+                {
+                    name: "PORCINI",
+                    ingredients: ["Pomodoro", "mozzarella", "funghi porcini"]
+                },
+                {
+                    name: "PROSCIUTTO E FUNGHI",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto cotto", "funghi"]
+                },
+                {
+                    name: "PUGLIESE",
+                    ingredients: ["Pomodoro", "mozzarella", "cipolle", "origano"]
+                },
+                {
+                    name: "REGINA",
+                    ingredients: ["Pomodoro", "mozzarella", "wurstel"]
+                },
+                {
+                    name: "4 FORMAGGI",
+                    ingredients: ["Pomodoro", "mozzarella", "zucchine", "melanzane", "scamorza"]
+                },
+                {
+                    name: "RICCA",
+                    ingredients: ["Pomodoro", "mozzarella", "rucola", "scamorza", "speck"]
+                },
+                {
+                    name: "4 STAGIONI",
+                    ingredients: ["Pomodoro", "mozzarella", "cotto", "carciofi", "funghi", "olive"]
+                },
+                {
+                    name: "ZOLA",
+                    ingredients: ["Pomodoro", "mozzarella", "zola"]
+                },
+                {
+                    name: "ZUCCHINE E GAMBERI",
+                    ingredients: ["Pomodoro", "mozzarella", "zucchine", "gamberi"]
+                },
+            ],
+        },
+        calzoni: {
+            header: "I NOSTRI CALZONI",
+            items: [
+                {
+                    name: "CALZONE LISCIO",
+                    ingredients: ["Pomodoro", "mozzarella", "prosciutto cotto"]
+                },
+                {
+                    name: "CALZONE FARCITO",
+                    ingredients: ["Pomodoro", "mozzarella", "cotto", "funghi", "carciofi"]
+                },
+                {
+                    name: "CALZONE CON NUTELLA",
+                    ingredients: [],
+                },
+            ],
+        },
+        focacce: {
+            header: "FOCACCE",
+            items: [
+                {
+                    name: "FOCACCE A SCELTA",
+                    ingredients: [],
+                },
+            ]
+        },
+        pizzaFamiglia: {
+            header: "PIZZA FAMIGLIA",
+            items: [
+                {
+                    name: "TEGLIA MARGHERITA",
+                    ingredients: [],
+                },
+                {
+                    name: "TEGLIA 1/2 META MARGHERITA",
+                    ingredients: [],
+                },
+                {
+                    name: "MET TEGLIA FARCITA",
+                    ingredients: [],
+                },
+
+            ]
+        },
+    }
+];
